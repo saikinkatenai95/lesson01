1d4870da04b60d624b062be7b59fae84f83d2d82
saikinkatenai95 <saikinkatenai95@outlook.jp>
2022-01-05 18:37:06 +0900

    first commit

---
+++ sample04.php
@@ -0,0 +1,3 @@
+<?php
+date_default_timezone_set('Asia/Tokyo');
+echo date('G時 i分 s秒');
---
+++ sample08.php
@@ -0,0 +1,8 @@
+<?php
+for($i=1; $i<366; $i++):
+  echo $i .'<br>';
+endfor;
+?>
+
+for(初期化; 繰り返し条件; 更新処理):
+endfor;
